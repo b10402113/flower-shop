2623a136fbdc715941f621d054c2ae921ba8e7bd
Andy Cheng <b10402113@gmail.com>
2021-06-29 16:10:01 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
-  // devServer: {
-  //   host: 'localhost',
-  //   port: 8080,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://140.118.122.149:8000',
-  //       changeOrigin: true,
-  //       pathRewrite: {
-  //         '/api': ''
-  //       }
-  //     }
-  //   }
-  // }
 }
